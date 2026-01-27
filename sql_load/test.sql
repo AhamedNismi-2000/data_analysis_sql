0cd799d93c5c74a760f511abd8ed90e60835c8eb
Ahamed Nismi <mnanismi2000@gmail.com>
2026-01-27 13:02:13 +0530

    grouped fuction

--- sql_load/test.sql
+++ sql_load/test.sql
@@ -1,11 +1,31 @@
 
-DROP TABLE job_postings_fact,
-          skills_job_dim,
-          skills_dim,
-          company_dim;
+SELECT 
+       COUNT(job_id) AS total_job_postings,
+       EXTRACT(MONTH FROM job_posted_date) AS month
+FROM job_postings_fact
+WHERE job_title_short = 'Data Scientist'
+GROUP BY month
+ORDER BY total_job_postings DESC;
+
+
+
+
+
+
 
 
 SELECT * FROM company_dim LIMIT 5;
 SELECT * FROM skills_dim LIMIT 5;
 SELECT * FROM job_postings_fact LIMIT 5;
-SELECT * FROM skills_job_dim LIMIT 5;          
+SELECT * FROM skills_job_dim LIMIT 5;    
+
+
+/*DROP TABLE job_postings_fact,
+          skills_job_dim,
+          skills_dim,
+          company_dim;
+*/
+
+      
+
+
