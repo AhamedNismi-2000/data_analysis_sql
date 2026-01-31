fdcd2e00a16a6ecfddf97b04e4373fc93f3a5b59
Ahamed Nismi <mnanismi2000@gmail.com>
2026-01-31 13:20:34 +0530

    Top Skills For Data Analyst

--- project_1/top_demanded_skills.sql
+++ project_1/top_demanded_skills.sql
@@ -7,6 +7,7 @@
 providing insights into the most valuable skills for job seekers.
 */
 
+-- This is Top Demanded Skills for Data Scientist 
 SELECT 
     sd.skills AS skill_name,
     COUNT(sjd.job_id) AS skill_count,
@@ -23,3 +24,25 @@ SELECT
     jpf.salary_year_avg 
     ORDER BY skill_count DESC
     LIMIT 20;
+
+-- This Top Demanded Skills For Data Analyst 
+SELECT 
+    sd.skills AS skill_name,
+    COUNT(sjd.job_id) AS skill_count,
+    jpf.salary_year_avg
+    FROM job_postings_fact AS jpf
+    INNER JOIN skills_job_dim as sjd
+    ON jpf.job_id = sjd.job_id
+    INNER JOIN skills_dim AS sd 
+    ON  sjd.skill_id = sd.skill_id
+    WHERE jpf.job_title_short = 'Data Analyst' AND 
+    salary_year_avg IS NOT NULL  AND 
+    job_work_from_home = TRUE
+    GROUP BY skill_name ,
+    jpf.salary_year_avg 
+    ORDER BY skill_count DESC
+    LIMIT 20;
+
+
+
+    
